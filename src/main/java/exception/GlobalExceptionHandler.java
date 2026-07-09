8767e0ea27da940be3dd75d083b0682045a74154
AndreiMit23 <andrei.m5@yahoo.com>
2026-07-09 19:00:22 +0300

    refactor: extract entity mapping into TaskMapper

--- src/main/java/exception/GlobalExceptionHandler.java
+++ src/main/java/exception/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package exception;
 
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.NoSuchElementException;
 
@@ -19,6 +21,11 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalStateException.class)
     public String handleIllegalState(IllegalStateException exception){
+        return exception.getMessage();
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public String handleNotFound(ResourceNotFoundException exception) {
         return exception.getMessage();
     }
 }
